04ea72ef80a608308df1f71a2b22a8f9999344c5
EgorAstakhov <60236365+EgorAstakhov@users.noreply.github.com>
2020-04-19 13:56:28 +0400

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,102 +1,48 @@
 from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
 
 
-class Paint(Frame):
 
-    def __init__(self, parent):
-        Frame.__init__(self, parent)
-
-        self.parent = parent
-        self.color = "black"
-        self.brush_size = 2
-        self.setUI()
-
-    def set_color(self, new_color):
-        self.color = new_color
-
-    def set_brush_size(self, new_size):
-        self.brush_size = new_size
-
-    def draw(self, event):
-        self.canv.create_oval(event.x - self.brush_size,
-                              event.y - self.brush_size,
-                              event.x + self.brush_size,
-                              event.y + self.brush_size,
-                              fill=self.color, outline=self.color)
-
-    def setUI(self):
-
-        self.parent.title("Paint")  
-        self.pack(fill=BOTH, expand=1)  
-
-        self.columnconfigure(6, weight=1) 
-        self.rowconfigure(2, weight=1) 
-
-        self.canv = Canvas(self, bg="white")  
-        self.canv.grid(row=2, column=0, columnspan=7,
-                       padx=5, pady=5, sticky=E+W+S+N)  
-        self.canv.bind("<B1-Motion>", self.draw) 
-
-        color_lab = Label(self, text="Color: ") 
-        color_lab.grid(row=0, column=0, padx=6) 
-
-        red_btn = Button(self, text="Red", width=10,
-                         command=lambda: self.set_color("red")) 
-        red_btn.grid(row=0, column=1) 
-
-        green_btn = Button(self, text="Green", width=10,
-                           command=lambda: self.set_color("green"))
-        green_btn.grid(row=0, column=2)
-
-        blue_btn = Button(self, text="Blue", width=10,
-                          command=lambda: self.set_color("blue"))
-        blue_btn.grid(row=0, column=3)
-
-        black_btn = Button(self, text="Black", width=10,
-                           command=lambda: self.set_color("black"))
-        black_btn.grid(row=0, column=4)
-
-        white_btn = Button(self, text="White", width=10,
-                           command=lambda: self.set_color("white"))
-        white_btn.grid(row=0, column=5)
-
-        clear_btn = Button(self, text="Clear all", width=10,
-                           command=lambda: self.canv.delete("all"))
-        clear_btn.grid(row=0, column=6, sticky=W)
-
-        size_lab = Label(self, text="Brush size: ")
-        size_lab.grid(row=1, column=0, padx=5)
-        one_btn = Button(self, text="Two", width=10,
-                         command=lambda: self.set_brush_size(2))
-        one_btn.grid(row=1, column=1)
-
-        two_btn = Button(self, text="Five", width=10,
-                         command=lambda: self.set_brush_size(5))
-        two_btn.grid(row=1, column=2)
-
-        five_btn = Button(self, text="Seven", width=10,
-                          command=lambda: self.set_brush_size(7))
-        five_btn.grid(row=1, column=3)
-
-        seven_btn = Button(self, text="Ten", width=10,
-                           command=lambda: self.set_brush_size(10))
-        seven_btn.grid(row=1, column=4)
-
-        ten_btn = Button(self, text="Twenty", width=10,
-                         command=lambda: self.set_brush_size(20))
-        ten_btn.grid(row=1, column=5)
-
-        twenty_btn = Button(self, text="Fifty", width=10,
-                            command=lambda: self.set_brush_size(50))
-        twenty_btn.grid(row=1, column=6, sticky=W)
+def delete():
+    selection = languages_listbox.curselection()  
+    mes = messagebox.askyesno(title="Вопрос", message="Удалить данные?")  
+    if mes == TRUE: 
+        languages_listbox.delete(selection[0])
 
 
-def main():
-    root = Tk()
-    root.geometry("850x500+300+300")
-    app = Paint(root)
-    root.mainloop()
+
+def add():
+    new_language = language_entry.get()  
+    languages_listbox.insert(0, new_language) 
+    writer = open('language.txt', 'a')
+    writer.write(' ' + str(new_language))
+    writer.close()
 
 
-if __name__ == '__main__':
-    main()
+root = Tk()
+root.title("GUI на Python")
+
+
+language_entry = Entry(width=40)  
+language_entry.grid(column=0, row=0, padx=6, pady=6)
+add_button = ttk.Button(text="Добавить", command=add).grid(column=1, row=0, padx=6, pady=6)
+
+
+languages_listbox = Listbox()
+languages_listbox.grid(row=1, column=0, columnspan=2, sticky=W + E, padx=5, pady=5)
+
+
+
+writer = open('language.txt', 'a')
+writer.write(' ')
+writer = open('language.txt')
+for line in writer:
+    words = line.split()
+for i in range(len(words)):
+    languages_listbox.insert(END,words[i])
+writer.close()
+
+delete_button = ttk.Button(text="Удалить", command=delete).grid(row=2, column=1, padx=5, pady=5)
+
+root.mainloop()
